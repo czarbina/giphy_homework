767a73cc73bdfd7d8592353dc21250d89d237515
Sabrina Kesler <sabrina@getvictorious.com>
2017-11-06 16:37:31 -0800

    Added div tag to hold buttons. Added form to take in new button input.

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 $(function() {
-	
+
 	var topics = ["Octopus", "Squid", "Cuttlefish", "Sunfish", "Whale Shark", "Manta Ray", "Mobula Ray",
 		"orca", "humpback whale", "narwhal", "blue whale", "beluga", "manatee"]
 
 		for (var i = 0; i<topics.length; i++) {
 			var buttons = $("<button>").text(topics[i]);
-			$("body").append(buttons);
+			$("#gif-buttons").append(buttons);
 
 		}
+
+	function displayGifs() {
+		var gif = $
+	}
+		
+
 	$("button").on("click", function() {
-		var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic 
+		+ "&limit=10&api_key=4TmD6bF0cg89gvwor3N8418yLrk2zN2X";
 
     	$.ajax({
       		url: queryURL,
       		method: 'GET'
     	}).done(function(response) {
+
+    		
       
     	});
 
 	});
 
-	alert("I'm connected!")
 });	 	
