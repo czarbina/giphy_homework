4248d1c29e184f96977b47c3fb89b02b4663ed11
Sabrina Kesler <sabrina@getvictorious.com>
2017-11-06 19:46:12 -0800

    For some reason I seem to do better when I start from scratch... Scrapped old html and js files and started building a different way. Got stills to populate and existing buttons to work.

--- app.js
+++ app.js
@@ -1,65 +1,42 @@
 $(function() {
-
-	var topics = ["Octopus", "Squid", "Cuttlefish", "Sunfish", "Whale Shark", "Manta Ray", "Mobula Ray",
-		"orca", "humpback whale", "narwhal", "blue whale", "beluga", "manatee"]
-
-		for (var i = 0; i<topics.length; i++) {
-			var buttons = $("<button>")
-			buttons.addClass("gif-button").attr("data-name", topics[i]).text(topics[i]);
-			$("#buttons-view").append(buttons);
-			console.log(buttons);
-
-		}
-
-	function displayGifs() {
 	
-		var gif = $(this).attr("data-name");
-		// var queryURL = "https://api.giphy.com/v1/gifs/search?q=manatee&limit=10&api_key=4TmD6bF0cg89gvwor3N8418yLrk2zN2X";
-		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif 
-		+ "&limit=10&api_key=4TmD6bF0cg89gvwor3N8418yLrk2zN2X";	
-
-		$.ajax({
-      		url: queryURL,
-      		method: 'GET'
-    	}).done(function(response) {
-
-    		var gifDiv = $("<div id='giffyDiv'>");
-    		var gifStill = response.data.images.fixed_height_still;
-    		var pStill = $("<img>").attr("src", gifStill); 
-    		var rating = response.data.rating;
-    		var pRating = $("<p>").text("Rating: " + rating);
-    		gifDiv.append(pRating);
-
-    		$("#gifs-view").prepend(gifDiv);
-      
-    	});
-
-	}
-
-	function makeButtons() {	
-
-	$("#buttons-view").empty();	
+	var topics = ["Octopus", "Squid", "Cuttlefish", "Sunfish", "Whale Shark", "Manta Ray", "Mobula Ray",
+		"orca", "humpback whale", "narwhal", "blue whale", "beluga", "manatee"];
 
 	for (var i = 0; i<topics.length; i++) {
 		var buttons = $("<button>")
 		buttons.addClass("gif-button").attr("data-name", topics[i]).text(topics[i]);
-		$("#buttons-view").append(buttons);
-		console.log(buttons);
+		$("#btn-row").append(buttons);
+	}
 
-		}
-	}	
+	$(".gif-button").on("click", function() {
 
-	$("#add-gif").on("click", function(event) {
-		event.preventDefault();
-		var gif = $("#gif-input").val().trim();
-		topics.push(gif);
-		makeButtons();
+	var gif = $(this).attr("data-name");
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&limit=10&api_key=4TmD6bF0cg89gvwor3N8418yLrk2zN2X";	
 
-	});
+	$.ajax({
+      	url: queryURL,
+      	method: 'GET'
+    }).done(function(response) {
 
-	$(document).on("click", "gif-button", displayGifs);
+		console.log(response);
+		var results = response.data;
 
-	
+		for (var i = 0; i< results.length; i++) {
+			var seaDiv = $("<div>");
+			var p = $("<p>").text("Rating: " + results[i].rating);
+			var animalStill = $("<img>");
+			animalStill.attr("src", results[i].images.fixed_height_still.url);
+			seaDiv.append(p);
+			seaDiv.append(animalStill);
+			$("#gif-dump-row").prepend(seaDiv);
+
+		}	
+    });	
 
 });
+
 	
+	console.log("Ready!");		
+
+});	
