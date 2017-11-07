4da0ee60e36639bb36ca539920bed96c84312aff
Sabrina Kesler <sabrina@getvictorious.com>
2017-11-06 17:46:18 -0800

    Hmmm can't get my images to append

--- app.js
+++ app.js
@@ -4,29 +4,62 @@ $(function() {
 		"orca", "humpback whale", "narwhal", "blue whale", "beluga", "manatee"]
 
 		for (var i = 0; i<topics.length; i++) {
-			var buttons = $("<button>").text(topics[i]);
-			$("#gif-buttons").append(buttons);
+			var buttons = $("<button>")
+			buttons.addClass("gif-button").attr("data-name", topics[i]).text(topics[i]);
+			$("#buttons-view").append(buttons);
+			console.log(buttons);
 
 		}
 
 	function displayGifs() {
-		var gif = $
-	}
-		
+	
+		var gif = $(this).attr("data-name");
+		// var queryURL = "https://api.giphy.com/v1/gifs/search?q=manatee&limit=10&api_key=4TmD6bF0cg89gvwor3N8418yLrk2zN2X";
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif 
+		+ "&limit=10&api_key=4TmD6bF0cg89gvwor3N8418yLrk2zN2X";	
 
-	$("button").on("click", function() {
-		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic 
-		+ "&limit=10&api_key=4TmD6bF0cg89gvwor3N8418yLrk2zN2X";
-
-    	$.ajax({
+		$.ajax({
       		url: queryURL,
       		method: 'GET'
     	}).done(function(response) {
 
-    		
+    		var gifDiv = $("<div id='giffyDiv'>");
+    		var gifStill = response.data.images.fixed_height_still;
+    		var pStill = $("<img>").attr("src", gifStill); 
+    		var rating = response.data.rating;
+    		var pRating = $("<p>").text("Rating: " + rating);
+    		gifDiv.append(pRating);
+
+    		$("#gifs-view").prepend(gifDiv);
       
     	});
 
+	}
+
+	function makeButtons() {	
+
+	$("#buttons-view").empty();	
+
+	for (var i = 0; i<topics.length; i++) {
+		var buttons = $("<button>")
+		buttons.addClass("gif-button").attr("data-name", topics[i]).text(topics[i]);
+		$("#buttons-view").append(buttons);
+		console.log(buttons);
+
+		}
+	}	
+
+	$("#add-gif").on("click", function(event) {
+		event.preventDefault();
+		var gif = $("#gif-input").val().trim();
+		topics.push(gif);
+		makeButtons();
+
 	});
 
-});	 	
+	$(document).on("click", "gif-button", displayGifs);
+
+	
+
+});
+	
